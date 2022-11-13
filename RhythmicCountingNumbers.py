e95e4c7002b5509592583001e816484307e5f490
MustafaSalici <mustafasalici@hotmail.com>
2022-11-13 20:01:56 +0300

    RhythmicCountingNumbers

---
+++ RhythmicCountingNumbers.py
@@ -0,0 +1,39 @@
+Result = [i for i in range(0,100,1)]   
+Result
+print(Result)
+
+Result = [i for i in range(0,100,2)]   
+Result
+print(Result)
+
+Result = [i for i in range(0,100,3)]   
+Result
+print(Result)
+
+Result = [i for i in range(0,100,4)]   
+Result
+print(Result)
+
+Result = [i for i in range(0,100,5)]   
+Result
+print(Result)
+
+Result = [i for i in range(0,100,6)]   
+Result
+print(Result)
+
+Result = [i for i in range(0,100,7)]   
+Result
+print(Result)
+
+Result = [i for i in range(0,100,8)]   
+Result
+print(Result)
+
+Result = [i for i in range(0,100,9)]   
+Result
+print(Result)
+
+Result = [i for i in range(0,100,10)]   
+Result
+print(Result)
