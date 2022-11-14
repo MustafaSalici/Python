5c8a1d0fc3efadf14329aad5a9723b012a6b7792
MustafaSalici <mustafasalici@hotmail.com>
2022-11-14 11:58:07 +0300

    CreateUserName

--- CreateUserName.py
+++ CreateUserName.py
@@ -1,16 +1,27 @@
-Name = input("Please enter your name: ")
-Surname = input("Please enter your surname: ")
+def main():
+    # Open the file to write the name and surname data in write mode
+    NameSurnameWrite = open("NameSurname.txt", "w")
 
-SubName = str(Name) + " " + str(Surname)
-SubName = SubName + "\n"
+    # Retrieving name and surname data from the user
+    NameSurnameValue = input("Enter your first and last name with a space in between (press enter to end): ")
+    while NameSurnameValue!="":
+        print(NameSurnameValue, file=NameSurnameWrite)
+        NameSurnameValue = input("Enter your first and last name with a space in between (press enter to end): ")
 
-w = open("Names.txt", "a+")
-w.writelines(SubName)
-w.close()
+    # Open name and surname data in read mode
+    NameSurnameWrite = open("NameSurname.txt", "r")
 
-UserName = Name[0] + Surname + "\n"
-print(UserName)
+    # Open file to write usernames in write mode
+    UserNameValue = open("UserName.txt", "w")
 
-r = open("Users.txt", "a+")
-r.writelines(UserName)
-print(r.read())
+    # Get lines from file with name and surname data
+    for line in NameSurnameWrite:
+        # Split line by spaces and put in a list
+        NameSurnameList = line.split(" ")
+
+        # First element of the list name second element last name
+        # Combine the first letter of the name with the last name and write it in the username list
+        user = NameSurnameList[0][0] + NameSurnameList[1]
+        print(user, file=UserNameValue)
+
+main()
